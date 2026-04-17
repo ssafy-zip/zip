0a17634fc6d6ca2525756d546985bffd75727d85
BAE JUNSU <jsbae1023@gmail.com>
2026-04-17 14:56:01 +0900

    docs: sql 추가

---
+++ sql/ssafyhome_apartment.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ssafyhome
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `apartment`
+--
+
+DROP TABLE IF EXISTS `apartment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `apartment` (
+  `build_year` int DEFAULT NULL,
+  `bonbun` varchar(10) DEFAULT NULL,
+  `bubun` varchar(10) DEFAULT NULL,
+  `land_cd` varchar(10) DEFAULT NULL,
+  `road_nm_bonbun` varchar(10) DEFAULT NULL,
+  `road_nm_bubun` varchar(10) DEFAULT NULL,
+  `road_nm_seq` varchar(10) DEFAULT NULL,
+  `road_nm_sgg_cd` varchar(10) DEFAULT NULL,
+  `road_nmb_cd` varchar(10) DEFAULT NULL,
+  `sgg_cd` varchar(10) DEFAULT NULL,
+  `umd_cd` varchar(10) DEFAULT NULL,
+  `road_nm_cd` varchar(20) DEFAULT NULL,
+  `apt_seq` varchar(50) NOT NULL,
+  `umd_nm` varchar(100) DEFAULT NULL,
+  `apt_nm` varchar(200) NOT NULL,
+  `road_nm` varchar(200) DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`apt_seq`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-27 17:20:59
